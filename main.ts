30fc277c4c8b952275e65ec3dbc9ff67e108e0a5
erichon <erichon18@gmail.com>
2022-04-15 18:17:21 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+})
+radio.setGroup(1)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+    radio.sendValue("x", pins.analogReadPin(AnalogPin.P2))
+    radio.sendValue("y", pins.analogReadPin(AnalogPin.P1))
+})
