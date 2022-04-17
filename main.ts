352981c5e31a5982083ec20c46539884b4cbd8f1
erichon <erichon18@gmail.com>
2022-04-17 09:31:56 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,6 +16,8 @@ input.onButtonPressed(Button.B, function () {
         . . . . .
         `)
 })
+let curr_p2 = 0
+let curr_p1 = 0
 radio.setGroup(1)
 basic.showLeds(`
     . . . . .
@@ -25,8 +27,20 @@ basic.showLeds(`
     . . . . .
     `)
 basic.forever(function () {
-    radio.sendValue("x", pins.analogReadPin(AnalogPin.P2))
-    radio.sendValue("y", pins.analogReadPin(AnalogPin.P1))
+    curr_p1 = pins.analogReadPin(AnalogPin.P1)
+    radio.sendValue("y", curr_p1)
+    if (curr_p1 > 450 && curr_p1 < 550) {
+        if (curr_p2 > 450 && curr_p2 < 550) {
+            radio.sendValue("stop", 1)
+        }
+    }
+    curr_p2 = pins.analogReadPin(AnalogPin.P2)
+    radio.sendValue("x", curr_p2)
+    if (curr_p2 > 450 && curr_p2 < 550) {
+        if (curr_p1 > 450 && curr_p1 < 550) {
+            radio.sendValue("stop", 1)
+        }
+    }
     if (pins.digitalReadPin(DigitalPin.P12) == 0) {
         radio.sendValue("stop", 1)
     }
